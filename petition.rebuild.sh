25dafe2d21566dd45d0940dee545de5fed8cb994
maxwellhansen <maxwellhansen@hotmail.com>
2012-08-24 14:18:49 -0500

    Update petition.rebuild.sh
    Replaced -R option of rm with the functionally equivalent, but vastly more common -r option.

--- petition.rebuild.sh
+++ petition.rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Rebuilding..."
-rm -Rf modules/contrib themes/tao themes/rubik libraries/
+rm -rf modules/contrib themes/tao themes/rubik libraries/
 drush -y make --no-core --contrib-destination=. drupal-org.make
