0127406438c509b0e50fc376fdc278aaf624c794
David Stoline <David_D._Stoline@oa.eop.gov>
2012-08-28 20:51:40 +0000

    PT-508, Update maintenance-page.tpl.php, add support for maintenance200.

--- themes/petitions/pages/maintenance-page.tpl.php
+++ themes/petitions/pages/maintenance-page.tpl.php
@@ -1,7 +1,3 @@
-<?php
-drupal_add_http_header('Status', '200 OK');
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="en" dir="ltr">
