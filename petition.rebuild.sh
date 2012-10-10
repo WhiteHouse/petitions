342bf1c2ce65612365dad7226aaf0e6e7407cbdd
Bryan Hirsch <bryan_s_hirsch@oa.eop.gov>
2012-10-10 11:13:26 -0400

    Update petition.rebuild.sh.

--- petition.rebuild.sh
+++ petition.rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Rebuilding..."
-rm -rf modules/contrib themes/tao themes/rubik libraries/
+rm -rf modules/contrib themes/tao themes/rubik themes/zen themes/fortyfour libraries/
 drush -y make --no-core --contrib-destination=. drupal-org.make
