7d3472e9f5391b0abe341ccc6e4ffdb745a4e91d
TravisCarden <Travis_R_Carden@oa.eop.gov>
2014-04-17 11:00:32 -0500

    PT-1316 - Renamed hook_signatures_queue_subscribe() to hook_petition_signatory_opt_in() and documented.

---
+++ modules/custom/petitions_api/signatures_queue/signatures_queue.api.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Signatures Queue module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Respond to a petition signatory opting in.
+ *
+ * This hook is invoked whenever a petition is signed with the "signup" flag.
+ *
+ * @param array $signatory
+ *   An associative array of signatory details:
+ *   - first_name: First name.
+ *   - last_name: Last name.
+ *   - email: Email address.
+ *   - zip: Zip code.
+ */
+function hook_petition_signatory_opt_in(array $signatory) {
+  // Save the signatory details to a database, add them to a mailing list, or
+  // otherwise capture their record here.
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
