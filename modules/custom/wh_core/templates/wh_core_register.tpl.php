9c4b940f98438325ea980044dff6fe3989ec51fc
TravisCarden <Travis_R_Carden@oa.eop.gov>
2014-06-23 10:47:10 -0500

    PT-1400 - Updated help text on user registration screen.

--- modules/custom/wh_core/templates/wh_core_register.tpl.php
+++ modules/custom/wh_core/templates/wh_core_register.tpl.php
@@ -1,12 +1,14 @@
-<?php 
-/**
-*
-* Variables provided by wh_core:
-*  - $wh_core_register, available pre_process function to pass variables to this TPL. 
-*
-*/
-?>
+<?php
 
+/**
+ * @file
+ * Default theme implementation of the user registration form help text.
+ *
+ * Variables provided by wh_core:
+ *  - wh_core_register: Available pre_process function to pass variables to this
+ *    template.
+ */
+?>
 <div id="user-register-form-wrapper">
   <h1>Create a WhiteHouse.gov Account</h1>
   <div class="webform-description">
@@ -14,7 +16,7 @@
     <ol>
       <li><b>Create an Account.</b> Fill out the form below to create an account.</li>
       <li><b>Verify Your Account.</b> Within a few minutes and email will be sent to you.  You must click on the link in that email in order to verify your account</li>
-      <li><b>Create or Sign a Petition.</b> Once you've verified your account you can create and sign petitions.  Note that you must click the green "Sign this Petition" button on the petition you wish to sign after you've verified your account in order for your signature to be counted.</li>
+      <li><b>Create or Sign a Petition.</b> Once you've verified your account you can create petitions.</li>
     </ol>
   </div><!--/web form description-->
 </div><!--/user register form wrapper -->
